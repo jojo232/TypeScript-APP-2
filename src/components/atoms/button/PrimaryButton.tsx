3113c708fd562dc8ada9e343c02fa67c247003c5
Yuya-Sasaki <65487059+jojo232@users.noreply.github.com>
2023-05-17 09:48:45 +0900

    Update PrimaryButton.tsx

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -23,3 +23,15 @@ export const PrimaryButton: VFC<Props> = memo((props) => {
     </Button>
   );
 });
+
+-------------------------------------------------
+  
+  memo 関数は、コンポーネントをメモ化して再レンダリングのパフォーマンスを最適化するために使用されます。
+ReactNode 型は、React コンポーネントの子要素として許容される要素の型を表します。
+VFC (View Function Component) は、React の関数コンポーネントの型です。
+Button は Chakra UI ライブラリからインポートされたコンポーネントです。
+Props 型は、PrimaryButton コンポーネントが受け取ることができるプロパティを指定します。children は任意の ReactNode 型の子要素であり、disabled と loading はオプションのブール型のプロパティです。onClick は戻り値を返さない関数として指定されています。
+
+PrimaryButton コンポーネントは、Button コンポーネントをラップしています。渡されたプロパティを適用し、children を表示します。disabled や loading の値に応じて、ボタンの状態を制御します。
+
+このコンポーネントは、VFC<Props> として定義されており、Props 型を持つプロパティを受け取ることができます。また、memo 関数によってメモ化されています。これにより、プロパティが変更されない限り再レンダリングが行われません。
